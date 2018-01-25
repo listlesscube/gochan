c9a505f06f3bf43fe8d5be6e60831915ab61ed58
Listless Cube <listlesscube@airmail.cc>
2018-01-24 18:34:49 -0700

    Make members of Board public

--- boards.go
+++ boards.go
@@ -3,22 +3,22 @@ package gochan
 // Board is a description of a 4chan board. Board's fields are equivalent
 // to JSON returned by https://a.4chan.org/boards.json
 type Board struct {
-	board           string
-	title           string
-	wsBoard         bool
-	perPage         int
-	pages           int
-	maxFilesize     int
-	maxWebmFilesize int
-	maxCommentChars int
-	maxWebmDuration int
-	bumpLimit       int
-	imageLimit      int
-	cooldowns       struct {
-		threads int
-		replies int
-		images  int
+	Board           string
+	Title           string
+	WSBoard         bool
+	PerPage         int
+	Pages           int
+	MaxFilesize     int
+	MaxWebMFilesize int
+	MaxCommentChars int
+	MaxWebMDuration int
+	BumpLimit       int
+	ImageLimit      int
+	Cooldowns       struct {
+		Threads int
+		Replies int
+		Images  int
 	}
-	metaDescription string
-	isArchived      bool
+	MetaDescription string
+	IsArchived      bool
 }
