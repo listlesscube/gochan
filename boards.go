fb64f6852557aefbfca8e42fc402389fb0196d7c
Listless Cube <listlesscube@airmail.cc>
2018-01-24 19:34:48 -0700

    Add function GetBoards and json tags to struct Board

--- boards.go
+++ boards.go
@@ -1,24 +1,55 @@
 package gochan
 
+import (
+	"encoding/json"
+)
+
 // Board is a description of a 4chan board. Board's fields are equivalent
 // to JSON returned by https://a.4cdn.org/boards.json
 type Board struct {
 	Board           string
 	Title           string
-	WSBoard         bool
-	PerPage         int
+	WSBoard         bool `json:"ws_board"`
+	PerPage         int  `json:"per_page"`
 	Pages           int
-	MaxFilesize     int
-	MaxWebMFilesize int
-	MaxCommentChars int
-	MaxWebMDuration int
-	BumpLimit       int
-	ImageLimit      int
+	MaxFilesize     int `json:"max_filesize"`
+	MaxWebMFilesize int `json:"max_webm_filesize"`
+	MaxCommentChars int `json:"max_comment_chars"`
+	MaxWebMDuration int `json:"max_webm_duration"`
+	BumpLimit       int `json:"bump_limit"`
+	ImageLimit      int `json:"image_limit"`
 	Cooldowns       struct {
 		Threads int
 		Replies int
 		Images  int
 	}
-	MetaDescription string
-	IsArchived      bool
+	MetaDescription string `json:"meta_description"`
+	IsArchived      bool   `json:"is_archived"`
+}
+
+type boardJSON struct {
+	Boards []Board
+}
+
+// GetBoards returns a slice containing all boards. An error is returned if an
+// error is encountered while attempting to acquire the list of boards.
+func GetBoards() ([]Board, error) {
+	resp, err := client.Get("https://a.4cdn.org/boards.json")
+	if err != nil {
+		return nil, err
+	}
+
+	decoder := json.NewDecoder(resp.Body)
+	_, err = decoder.Token()
+	if err != nil {
+		return nil, err
+	}
+
+	var boards []Board
+	err = decoder.Decode(&boards)
+	if err != nil {
+		return nil, err
+	}
+
+	return boards, nil
 }
--- gochan.go
+++ gochan.go
@@ -1,3 +1,7 @@
 // Package gochan is currently in development. A lot of functionality is missing or may
 // be broken. Use at your own risk.
 package gochan
+
+import "net/http"
+
+var client = &http.Client{}
